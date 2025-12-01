86a53bc38c2a1dd4641c2d46da039f385adf62d2
Emilio Righi <righiemilio@gmail.com>
2025-12-01 16:47:14 +0100

    patch add jobs to celery config

--- server/celery_app/celery_worker.py
+++ server/celery_app/celery_worker.py
@@ -1,7 +1,7 @@
 from .celery_utils import create_celery
 from db.database import connect_to_db
 from jobs.import_annotations import import_annotations
-from jobs.updates import update_assembly_fields, update_annotation_fields
+from jobs.updates import update_assembly_fields, update_annotation_fields, update_feature_stats, update_bioprojects
 
 app = create_celery()
 
